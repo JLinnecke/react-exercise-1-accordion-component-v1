5a2a5c473a8a3640173c21536dedea566e76bc61
🎭 🎪 <jlinnecke@gmail.com>
2026-06-08 12:03:11 +0200

    Add accordion toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import { useState } from "react";
 
 const faqs = [
   {
@@ -34,12 +35,18 @@ function Accordion({ data }) {
 }
 
 function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
-    <div className="item">
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
